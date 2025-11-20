2b27b2bcc6fc6261fe1638e9fa70ed9aeaaf54cc
Bhargav-local <bhargavindian@outlook.com>
2025-11-20 15:57:01 +0530

    Added Starter Code

---
+++ src/test/java/com/example/kafka/kafka_tutorial/KafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka.kafka_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
